bcb1e8ef8eb540a51fd10d536ccdf4afcce9bc0c
Chai <chch22@patana.ac.th>
2019-11-14 20:51:48 +0700

    Added Installer

---
+++ installer/mac-installer.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main ()
+{
+    printf("\n\n\n"
+    "* * * * * * * * * * * * * * * * * * * * *\n"
+    "*                                       *\n"
+    "* Chai112's Boeing 777-300ER Installer  *\n"
+    "*                                       *\n"
+    "* * * * * * * * * * * * * * * * * * * * *\n"
+    "\n\n\n");
+    return 0;
+}
